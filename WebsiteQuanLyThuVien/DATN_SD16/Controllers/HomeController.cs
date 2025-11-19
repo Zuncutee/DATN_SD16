0db3c3e9d55de356598c0375f213c1ab62fe7838
VuDung <zunisme@gmail.com>
2025-11-19 10:02:43 +0700

    setup solution để code

---
+++ WebsiteQuanLyThuVien/DATN_SD16/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using DATN_SD16.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_SD16.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
